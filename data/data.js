e33858581c29cc828be1d0651f211e8829fb2847
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-01-28 04:06:58 +0100

    Update portfolio projects

--- data/data.js
+++ data/data.js
@@ -1,18 +1,7 @@
 exports.data = [
   {
     id: 1,
-    name: 'Dfweb versjon 2',
-    description: 'Nåværende portefølje med Gatsby og React.',
-    subdescription: 'Gatsby, GSAP, Animate.css, Tailwind CSS, Cypress.',
-    category: 'React',
-    urlwww: 'https://www.dfweb.no/',
-    urlgithub: 'https://github.com/w3bdesign/dfweb',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588559314/Dfweb/dfweb-v2_g9wjyw.jpg',
-  },
-  {
-    id: 2,
-    name: 'Nextjs WooCommerce',
+    name: 'NextJS WooCommerce',
     description: 'Nettbutikk med WooCommerce som backend.',
     subdescription:
       'NextJS, React, GraphQL, Tailwind CSS, React Hook Form med mer.',
@@ -23,7 +12,42 @@ exports.data = [
       'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1590539031/Dfweb/nextjs-woocommerce_kknqkg.jpg',
   },
   {
+    id: 2,
+    name: 'NuxtJS WooCommerce',
+    description: 'Nettbutikk med Nuxt og Vue.',
+    subdescription:
+      'Vue, Nuxt, Tailwind, Vue Formulate, animasjoner, GraphQL, Algolia.',
+    category: 'Vue',
+    urlwww: 'https://nuxt.dfweb.no/',
+    urlgithub: 'https://github.com/w3bdesign/nuxtjs-woocommerce',
+    image:
+      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1607058540/Dfweb/nuxtwoo_ymrolw.jpg',
+  },
+  {
     id: 3,
+    name: 'Dfweb versjon 2',
+    description: 'Nåværende portefølje med Gatsby og React.',
+    subdescription: 'Gatsby, GSAP, Animate.css, Tailwind CSS, Cypress.',
+    category: 'React',
+    urlwww: 'https://www.dfweb.no/',
+    urlgithub: 'https://github.com/w3bdesign/dfweb',
+    image:
+      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588559314/Dfweb/dfweb-v2_g9wjyw.jpg',
+  },
+
+  {
+    id: 4,
+    name: 'Algolia Woo Indexer',
+    description: 'Plugin for å sende WooCommerce-produkter til Algolia.',
+    subdescription: 'WordPress, PHP, OOP, PHPCS, POT',
+    category: 'PHP',
+    urlwww: 'https://github.com/w3bdesign/algolia-woo-indexer/',
+    urlgithub: 'https://github.com/w3bdesign/algolia-woo-indexer/',
+    image:
+      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1594424771/Dfweb/algolia-woocommerce-indexer.jpg',
+  },
+  {
+    id: 5,
     name: 'Statens Vegvesen',
     description: 'Henter informasjon fra API med serverless backend.',
     subdescription:
@@ -35,84 +59,7 @@ exports.data = [
       'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588556573/Dfweb/statens-vegvesen_xsth2e.jpg',
   },
   {
-    id: 4,
-    name: 'Nettbutikk i React',
-    description: 'Nettbutikk laget i React.',
-    subdescription: 'React, Firebase, Redux, Styled components.',
-    category: 'React',
-    urlwww: 'https://react-ecommerce.dfweb.no/',
-    urlgithub: 'https://github.com/w3bdesign/react-ecommerce',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588556748/Dfweb/react-ecommerce_pxj0my.jpg',
-  },
-  {
-    id: 5,
-    name: 'TODO med React Hooks',
-    description: 'TODO applikasjon med React Hooks og Ant Design.',
-    subdescription: 'React, Ant Design, Hooks, React-Testing-Library.',
-    category: 'React',
-    urlwww: 'https://todohooks.dfweb.no/',
-    urlgithub: 'https://github.com/w3bdesign/todo-hooks',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588555098/Dfweb/todo-hooks_ectluc.jpg',
-  },
-  {
     id: 6,
-    name: 'DFWeb versjon 1',
-    description: 'Første versjon av www.dfweb.no.',
-    subdescription: 'HTML5, Javascript, SASS, GSAP',
-    category: 'Javascript',
-    urlwww: 'https://v1.dfweb.no',
-    urlgithub: 'https://github.com/w3bdesign/dfweb-v1',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588556839/Dfweb/dfweb-v1_txl9wx.jpg',
-  },
-  {
-    id: 7,
-    name: 'Bunadrosen',
-    description: 'Nettbutikk med WooCommerce og Pckasse integrasjon.',
-    subdescription: 'WordPress, WooCommerce, Pckasse',
-    category: 'WooCommerce',
-    urlwww: 'https://www.solvbutikken.no/',
-    urlgithub: '',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1595122726/Dfweb/solvbutikken_zrpjsf.jpg',
-  },
-  {
-    id: 8,
-    name: 'Algolia Woo Indexer',
-    description: 'Plugin for å sende WooCommerce-produkter til Algolia.',
-    subdescription: 'WordPress, PHP, OOP, PHPCS, POT',
-    category: 'WooCommerce',
-    urlwww: 'https://github.com/w3bdesign/algolia-woo-indexer/',
-    urlgithub: 'https://github.com/w3bdesign/algolia-woo-indexer/',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1594424771/Dfweb/algolia-woocommerce-indexer.jpg',
-  },
-  {
-    id: 9,
-    name: 'Nuxt WooCommerce',
-    description: 'Nettbutikk med Nuxt og Vue.',
-    subdescription: 'Vue, Nuxt, Tailwind, Vue Formulate, animasjoner, GraphQL, Algolia.',
-    category: 'Vue',
-    urlwww: 'https://nuxt.dfweb.no/',
-    urlgithub: 'https://github.com/w3bdesign/nuxtjs-woocommerce',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1607058540/Dfweb/nuxtwoo_ymrolw.jpg',     
-  },
-  {
-    id: 10,
-    name: 'TODO med Typescript',
-    description: 'TODO applikasjon med Material UI og Typescript.',
-    subdescription: 'React, Typescript, Material UI, Easy Peasy med mer',
-    category: 'React',
-    urlwww: 'https://react-todo-typescript.dfweb.no/',
-    urlgithub: 'https://github.com/w3bdesign/react-todo-typescript',
-    image:
-      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1597801368/Dfweb/todo-typescript-screenshot_kvmo1t.jpg',
-  },
-  {
-    id: 11,
     name: 'Stein, saks og papir',
     description: 'Stein, saks og papir i React.',
     subdescription:
@@ -122,5 +69,28 @@ exports.data = [
     urlgithub: 'https://github.com/w3bdesign/stein-saks-papir',
     image:
       'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1601217905/Dfweb/stein-saks-papir_w9joyf.jpg',
+  },
+  {
+    id: 7,
+    name: 'DFWeb versjon 1',
+    description: 'Første versjon av www.dfweb.no.',
+    subdescription: 'HTML5, Javascript, SASS, GSAP',
+    category: 'Javascript',
+    urlwww: 'https://v1.dfweb.no',
+    urlgithub: 'https://github.com/w3bdesign/dfweb-v1',
+    image:
+      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588556839/Dfweb/dfweb-v1_txl9wx.jpg',
+  },
+
+  {
+    id: 8,
+    name: 'TODO med Typescript',
+    description: 'TODO applikasjon med Material UI og Typescript.',
+    subdescription: 'React, Typescript, Material UI, Easy Peasy med mer',
+    category: 'React',
+    urlwww: 'https://react-todo-typescript.dfweb.no/',
+    urlgithub: 'https://github.com/w3bdesign/react-todo-typescript',
+    image:
+      'https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1597801368/Dfweb/todo-typescript-screenshot_kvmo1t.jpg',
   },
 ];
